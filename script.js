42a5e93ee4eea7b3bb059c1ea67680b8bcac449f
Lauro Cesar <lauroandradearaujo@gmail.com>
2020-11-05 20:40:33 -0300

    req1: dias do mes a lista de dias

--- script.js
+++ script.js
@@ -11,4 +11,28 @@ function creatDaysOfWeek() {
 }
 creatDaysOfWeek()
 
+// Requisito 1
+function createDaysOfTheMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const days = document.getElementById('days');
 
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const day = document.createElement('li');
+        const dayMonth = dezDaysList[index];
+
+        day.innerText = dayMonth;
+
+        if (dayMonth === 24 || dayMonth === 31) {
+            day.className = "day holiday";
+        } else if (dayMonth === 4 || dayMonth === 11 || dayMonth === 18) {
+            day.className = "day friday";
+        } else if (dayMonth === 25) {
+            day.className = "day holiday friday";
+        } else {
+            day.className = "day";
+        }
+
+        days.appendChild(day);
+    }
+}
+createDaysOfTheMonth();
